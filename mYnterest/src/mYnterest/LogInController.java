7b1759a64e537a141f68a4d5294d921f73c9c75e
manganarofabio <manganarofabio@gmail.com>
2015-11-19 17:55:34 +0100

    start

--- mYnterest/src/mYnterest/LogInController.java
+++ mYnterest/src/mYnterest/LogInController.java
@@ -1,5 +1,25 @@
 package mYnterest;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LogInController {
-	
+
+    @FXML
+    private TextField txtUser;
+
+    @FXML
+    private PasswordField txtPass;
+
+    @FXML
+    private Button btnSignIn;
+
+    @FXML
+    void signIn(ActionEvent event) {
+
+    }
+
 }
