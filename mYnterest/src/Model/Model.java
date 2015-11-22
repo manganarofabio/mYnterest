ce1056894e7614dba16e53573265c5fa835ec0df
Fabio <Fabio@192.168.1.106>
2015-11-22 12:00:36 +0100

    update newUser method

--- mYnterest/src/Model/Model.java
+++ mYnterest/src/Model/Model.java
@@ -2,6 +2,7 @@ package Model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -14,19 +15,33 @@ public class Model {
 	public boolean insertUser (User u) throws ClassNotFoundException, SQLException	{
 		Class.forName("org.sqlite.JDBC"); 
 		Connection con = DriverManager.getConnection("jdbc:sqlite:Utenti.db"); 
-		Statement stat = con.createStatement();
-	
-		ResultSet rs = stat.executeQuery("select * from Users where name='" + u.getName() + "';");
+//		Statement stat = con.createStatement();
 		
-		 if(rs.next())	
-			 {
+		String templateCheck = "select * from Users where name=?";//modello di query
+		String templateCreate = "insert into Users (name, password) VALUES (?,?)";
+
+		PreparedStatement statCheck = con.prepareStatement(templateCheck);
+		PreparedStatement statCreate = con.prepareStatement(templateCreate);
+		statCheck.setString(1,u.getName());
+		
+		
+	
+//		ResultSet rs = stat.executeQuery("select * from Users where name='" + u.getName() + "';");
+		
+		 if(statCheck.execute() == true){
+			 statCheck.close();
 			 con.close();
 			 return false;
+			 
 		 }
 		 else {
+			 statCreate.setString(1,u.getName());
+			 statCreate.setString(2,u.getPassword());
+			 statCreate.execute();
+			 
 		
-			stat.executeUpdate("insert into Users (name, password) VALUES ('" + u.getName() + "','" + u.getPassword() +"');");
-			
+			//stat.executeUpdate("insert into Users (name, password) VALUES ('" + u.getName() + "','" + u.getPassword() +"');");
+			 statCreate.close();
 			 con.close();
 			 return true;
 		 }
